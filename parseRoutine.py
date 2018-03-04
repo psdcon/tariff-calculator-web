30b118517ba16a689cb1dddc45154e8d88552c00
Paul Connolly <psdcon@gmail.com>
2018-03-04 16:40:18 +0000

    loading rotuine

---
+++ parseRoutine.py
@@ -0,0 +1,7 @@
+import json
+import csv
+from pathlib import Path
+
+routines = Path('C:\\Users\\psdco\\Documents\\Web\\tariff-cra\\src\\data\\databases\\tariff_routines.csv')
+skills = Path('C:\\Users\\psdco\\Documents\\Web\\tariff-cra\\src\\data\\tariff_skills.json')
+
